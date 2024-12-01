f8d85d85d26253086751db6939a9f2648ca91697
V M Vara Prakash <vmvaraprakash@Katuris-MacBook-Pro.local>
2024-12-01 14:27:23 -0600

    api update

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clear } from "../helper/localStorage";
 
 const axiosConfig = {
-  baseURL: "https://tripwise-prakashkaturi-1ee05ae86887.herokuapp.com/homepage",
+  baseURL: "https://tripwise-prakashkaturi-1ee05ae86887.herokuapp.com/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
